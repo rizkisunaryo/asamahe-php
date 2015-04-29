fadfa17f0639ddf156ec1a8ffe84c7340edd3593
Rizki Sunaryo <rizkisunaryo@gmail.com>
2015-04-29 20:05:55 +0800

    progress so far

--- res/js/asamahe-2.0.1.js
+++ res/js/asamahe-2.0.1.js
@@ -1,9 +1,33 @@
+var API_URL_PREFIX = 'http://asamahe.com:2903/api/';
 var WINDOW_WIDTH_XS = 768;
 var PULL_RIGHT_CLASS = 'pull-right';
 var BUTTON_CLASS = 'btn';
 
+var BOTTOM_TRIGGER = $('#bottomTrigger');
+
 var lastScrollTop = 0;
 var navbarStatus = 'showing';
+
+function getCurTimeStr() {
+	var d = new Date();
+  var Y = d.getFullYear();
+  var M = zeroized(d.getMonth()+1);
+  var D = zeroized(d.getDate());
+  var h = zeroized(d.getHours());
+  var m = zeroized(d.getMinutes());
+  var s = zeroized(d.getSeconds());
+  var ms = zeroized(d.getMilliseconds());
+  return formatTimeStr(Y+'/'+M+'/'+D+' '+h+':'+m+':'+s+'.'+ms);
+}
+
+function formatTimeStr(Y,M,D,h,m,s,ms) {
+	return Y+'/'+M+'/'+D+' '+h+':'+m+':'+s+'.'+ms;
+}
+
+function zeroized (pNumber) {
+  return pNumber<=9? '0'+pNumber : pNumber;
+}
+
 $(function() {
 	popupImg();
 	$(window).on('scroll', function(){
